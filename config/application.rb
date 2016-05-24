f184c4cf6021e94a713321ed138c7f9c19c079ae
James J. Goodhouse <jgoodhouse@renewfinancial.com>
2016-05-24 16:32:54 -0700

    add initializer

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module Taco
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # config.colorize_logging = false
+    # config.rails_semantic_logger.formatter = :json
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
---
+++ config/initializers/semantic_logger_formatter.rb
@@ -0,0 +1,37 @@
+SemanticLogger.appenders.first.formatter = Proc.new do |log|
+  colors      = SemanticLogger::AnsiColors
+  level_color = colors::LEVEL_MAP[log.level]
+
+  # Header with date, time, log level and process info
+  message     = "#{log.formatted_time} #{level_color}#{log.level_to_s}#{colors::CLEAR} [#{log.process_info}]"
+
+  # Tags
+  message << ' ' << log.tags.collect { |tag| "[#{level_color}#{tag}#{colors::CLEAR}]" }.join(' ') if log.tags && (log.tags.size > 0)
+
+  # Duration
+  message << " (#{colors::BOLD}#{log.duration_human}#{colors::CLEAR})" if log.duration
+
+  # Class / app name
+  message << " #{level_color}#{log.name}#{colors::CLEAR}"
+
+  # Log message
+  message << " -- #{log.message}" if log.message
+
+  # Payload: Colorize the payload if the AwesomePrint gem is loaded
+  if log.has_payload?
+    payload = log.payload
+    message << ' -- ' <<
+      if !defined?(AwesomePrint) || !payload.respond_to?(:ai)
+        payload.inspect
+      else
+        payload.ai(@ai_options) rescue payload.inspect
+      end
+  end
+
+  # Exceptions
+  if log.exception
+    message << " -- Exception: #{colors::BOLD}#{log.exception.class}: #{log.exception.message}#{colors::CLEAR}\n"
+    message << log.backtrace_to_s
+  end
+  message
+end
